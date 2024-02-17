f61bd7e344de6e523650375bcdf1ce76a0ba7ba5
Vladyslav Otroshchenko <vladmih1988@gmail.com>
2024-02-17 18:48:52 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 let headerHeight = $('.header').height()
+let screenSize = window.innerWidth
+let md = 768
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= headerHeight) {
-        $('.header').addClass('header_scrolled')
-    } else {
-        $('.header').removeClass('header_scrolled')
-    }
+window.addEventListener('resize', function () {
+    screenSize = window.innerWidth
+    checkMediaQuery()
 })
 
 const swiper = new Swiper('.swiper', {
@@ -21,12 +20,19 @@ const swiper = new Swiper('.swiper', {
     },
 })
 
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= headerHeight) {
+        $('.header').addClass('header_scrolled')
+    } else {
+        $('.header').removeClass('header_scrolled')
+    }
+})
+
 $('.header-burger').click(function () {
     $(this).toggleClass('header-burger_active')
     $('body').toggleClass('lock')
     $('.header').toggleClass('header_open')
     $('#menu').fadeToggle(200)
-    $('#searchForm').hide()
     if (!$(this).hasClass('header-burger_active')) {
         $('.header-burger__text').text('меню')
     } else {
@@ -34,20 +40,40 @@ $('.header-burger').click(function () {
     }
 })
 
-$('#searchOpen').click(function () {
-    $('#searchForm').fadeIn(300)
+$('.navigation__title').click(function () {
+    if (screenSize < md) {
+        $(this).next().slideToggle()
+    }
+})
+$(document).click(function (e) {
+    let search = $('#searchForm')
+    let btn = $('#searchOpen')
+    if (btn.has(e.target).length === 1 && screenSize > md) {
+        search.fadeIn(300)
+    }
+    if (
+        search.has(e.target).length === 0 &&
+        btn.has(e.target).length === 0 &&
+        screenSize > md
+    ) {
+        search.fadeOut(300)
+    }
+})
+
+$('.header__search-input').on('change paste keyup', function () {
+    let inpLenth = $(this).val().length
+    if (inpLenth >= 1) {
+        $('.header__search-drop').fadeIn(200)
+    } else {
+        $('.header__search-drop').fadeOut(200)
+    }
 })
 
 function checkMediaQuery() {
-    // If the inner width of the window is greater then 768px
-    if (window.innerWidth < 768) {
-        $('.navigation__title').click(function () {
-            $(this).next().slideToggle()
-        })
+    if (screenSize < md) {
         $('#menu .container').prepend($('#searchForm'))
     } else {
         $('.header__search').prepend($('#searchForm'))
     }
 }
-
 checkMediaQuery()
